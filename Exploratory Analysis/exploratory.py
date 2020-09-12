e1f2dad1dcc133f3fe18a7eabaee3f6f89306835
Ricardo Huapaya Rey <ricardo.huapaya@ucr.ac.cr>
2020-09-11 21:33:37 -0600

    Create exploratory.py
    New files thats starts the exploratory analysis.

---
+++ Exploratory Analysis/exploratory.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('planilla_clean.csv')
+df_profe = pd.read_csv('planilla_profesores .csv')
+df_split = pd.read_csv('dictom_planilla.csv')
